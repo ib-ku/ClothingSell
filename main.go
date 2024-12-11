5ee5f62e274bf1e903a586a5b172624b9f9cbd0c
Ermek <230867@astanait.edu.kz>
2024-12-11 14:17:08 +0500

    firstConnectToPostman

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-//MAIN
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Hello, server! This is JSON data from Postman."}`))
+}
+
+func main() {
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
