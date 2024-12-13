c9f57b84a412bc32f84c14fb430b116801442de2
Abylay <abylaymoldakhmet@gmail.com>
2024-12-13 19:30:42 +0500

    created post for setUser function

--- store/controller/userController.go
+++ store/controller/userController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"store/model"
@@ -16,4 +17,21 @@ func AllUsers(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit:All Users Hit")
 	view.RenderUsers(w, users)
 
+}
+
+func SetUser(w http.ResponseWriter, r *http.Request) {
+	var newUser model.User
+
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+
+	if err != nil {
+		http.Error(w, "Invalid user data", http.StatusBadRequest)
+		return
+	}
+
+	users = append(users, newUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
 }
--- store/main.go
+++ store/main.go
@@ -9,8 +9,10 @@ import (
 func handleRequests() {
 	http.HandleFunc("/", controller.HomePage)
 	http.HandleFunc("/allProducts", controller.AllProducts)
-	http.HandleFunc("/setProduct", controller.SetProduct)
 	http.HandleFunc("/allUsers", controller.AllUsers)
+
+	http.HandleFunc("/setProduct", controller.SetProduct)
+	http.HandleFunc("/setUser", controller.SetUser)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
